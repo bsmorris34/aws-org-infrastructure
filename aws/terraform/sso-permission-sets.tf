77f13bddceebd6dcf0e6162af20eb7d417bc4b9c
Brandon Morris <bmorris1@gmail.com>
2025-09-24 09:55:30 -0500

    fix: format Terraform configuration
    - Run terraform fmt to fix formatting issues
    - Ensure consistent code style for CI/CD pipeline

--- aws/terraform/sso-permission-sets.tf
+++ aws/terraform/sso-permission-sets.tf
@@ -129,7 +129,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "app_deployer_dynamodb" {
 resource "aws_ssoadmin_permission_set_inline_policy" "app_deployer_iam_policy" {
   instance_arn       = tolist(data.aws_ssoadmin_instances.main.arns)[0]
   permission_set_arn = aws_ssoadmin_permission_set.application_deployer.arn
-  
+
   inline_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
